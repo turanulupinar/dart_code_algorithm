6a8ff17259319aae28fbf8461926c75341908315
= <zekai.demir@fimkastore.com>
2024-02-01 22:51:42 +0300

    timer

--- lib/future_example/future_example.dart
+++ lib/future_example/future_example.dart
@@ -1,4 +1,5 @@
-import 'package:dart_code_algorithms/future_example/profile.dart';
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class FutureExamplePage extends StatefulWidget {
@@ -8,113 +9,54 @@ class FutureExamplePage extends StatefulWidget {
   State<FutureExamplePage> createState() => _FutureExamplePageState();
 }
 
-class _FutureExamplePageState extends State<FutureExamplePage> {
-  TotalTimeModel? item;
+class _FutureExamplePageState extends State<FutureExamplePage>
+    with SingleTickerProviderStateMixin {
+  bool paused = false;
+
+  Timer? _timer;
+  int second = 0;
+  int minute = 0;
+  late AnimationController controller;
+  late Animation<double> animation;
+
   @override
   void initState() {
-    init();
-
     super.initState();
-  }
+    controller = AnimationController(
+      vsync: this,
+      reverseDuration: const Duration(milliseconds: 200),
+      duration: const Duration(milliseconds: 200),
+    );
 
-  init() async {
-    setState(() {
-      item = TotalTimeModel(time: minute);
+    animation = Tween<double>(begin: 0.0, end: 1.0).animate(controller);
+
+    _timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
+      if (paused == false) {
+        setState(() {
+          second++;
+        });
+      }
     });
   }
 
-  Color? backgroundColorThenStart = Colors.purple;
-  late TextStyle? stil = Theme.of(context)
-      .textTheme
-      .titleLarge
-      ?.copyWith(color: Colors.black, fontSize: 50);
-
-  bool backgroundChange = false;
-  int currentNumber = 0;
-  int endNumber = 10;
-  int second = 0;
-  int minute = 0;
-  bool isDone = true;
-  int succesValue = 0;
-  int total = 0;
-  bool once = true;
-
-// toplam zaman
-
-  totalTime() {
-    setState(() {});
-    return minute;
+  startTimer() {
+    setState(() {
+      paused = !paused;
+    });
   }
 
-// saniye + dakika
-  Future secondFunction() async {
-    for (var i = 0; i < 5; i++) {
-      setState(() {
-        currentNumber++;
-        backgroundChange = true;
-      });
-      await Future.delayed(const Duration(seconds: 1));
-      if (i == 4) {
-        setState(() {
-          currentNumber = 0;
-          minute++;
-          i++;
-        });
-        setState(() {});
-      }
-    }
-    return;
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
   }
-
-// başarı etiketleri metodu
-
-  Future<int> successfullMethods() async {
-    if (minute == 3) {
-      succesValue++;
-      setState(() {});
-    }
-    if (minute == 5) {
-      setState(() {});
-      succesValue++;
-    }
-    if (minute == 8) {
-      succesValue++;
-    }
-    return succesValue;
-  }
-
-  // segmented fonksiyon:
-
-  Future stopTimer() async {}
-  Future deleteTimer() async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:
-          backgroundChange == false ? Colors.white : backgroundColorThenStart,
       appBar: AppBar(
         centerTitle: true,
-        actions: [
-          InkWell(
-            onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                      builder: (context) => ProfileDart(
-                            timeset: minute,
-                          )));
-            },
-            child: const CircleAvatar(
-              radius: 40,
-              backgroundColor: Colors.white,
-              child: Icon(Icons.person),
-            ),
-          )
-        ],
         title: const Text("Zamanlayıcı"),
-        backgroundColor:
-            backgroundChange == false ? Colors.white : backgroundColorThenStart,
       ),
       body: SizedBox(
         width: MediaQuery.of(context).size.width,
@@ -122,69 +64,19 @@ class _FutureExamplePageState extends State<FutureExamplePage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Center(
-                child: RichText(
-                    text: TextSpan(
-                        text: minute.toString(),
-                        style: stil,
-                        children: [
-                  TextSpan(text: ":", style: stil),
-                  TextSpan(text: currentNumber.toString(), style: stil)
-                ]))),
-            Text(item?.time.toString() ?? "null"),
+            Text("$second. Saniye"),
             const SizedBox(
-              height: 80,
+              height: 12,
             ),
-            succesValue > 0
-                ? SizedBox(
-                    height: 200,
-                    width: 200,
-                    child: GridView.count(
-                      primary: false,
-                      padding: const EdgeInsets.all(10),
-                      crossAxisSpacing: 10,
-                      mainAxisSpacing: 10,
-                      crossAxisCount: succesValue,
-                      children: [
-                        ...List.generate(succesValue, (int index) {
-                          return Container(
-                              decoration: BoxDecoration(
-                                borderRadius: BorderRadius.circular(20),
-                                color: Colors.teal[100],
-                              ),
-                              padding: const EdgeInsets.all(8),
-                              child: const Image(
-                                  image:
-                                      AssetImage("assets/image/cactus.png")));
-                        })
-                      ],
-                    ),
-                  )
-                : const SizedBox.shrink(),
-            Text(total.toString()),
-            const Spacer(),
-            once == true
-                ? ElevatedButton.icon(
-                    label: const Text("başlat"),
-                    onPressed: () async {
-                      once = false;
-                      for (var i = 0; i < endNumber; i++) {
-                        if (minute > 9) {
-                          return;
-                        }
-
-                        await secondFunction();
-                        setState(() {});
-                        successfullMethods();
-                        total = totalTime();
-                        init();
-                      }
-
-                      setState(() {});
-                    },
-                    icon: const Icon(Icons.play_arrow))
-                : ElevatedButton(
-                    onPressed: () {}, child: const Text("sıfırla")),
+            ElevatedButton.icon(
+              label: Text(paused ? "Başlat" : "Durdur"),
+              onPressed: () {
+                paused ? controller.forward() : controller.reverse();
+                startTimer();
+              },
+              icon: AnimatedIcon(
+                  icon: AnimatedIcons.play_pause, progress: animation),
+            ),
             const SizedBox(
               width: 50,
             ),
@@ -196,13 +88,13 @@ class _FutureExamplePageState extends State<FutureExamplePage> {
               child: Slider(
                 activeColor: Colors.red,
                 inactiveColor: Colors.black,
-                value: minute.toDouble(),
+                value: second.toDouble(),
                 divisions: 200,
                 min: 0,
-                max: endNumber.toDouble(),
+                max: 100,
                 onChanged: (double val) async {
                   setState(() {
-                    minute = val.toInt();
+                    second = val.toInt();
                   });
                 },
               ),
@@ -220,7 +112,6 @@ class TotalTimeModel {
     required this.time,
   });
 }
-
 
 // class CustomSegment extends StatelessWidget {
 //   const CustomSegment({super.key, required this.selected});
--- lib/future_example/profile.dart
+++ lib/future_example/profile.dart
@@ -1,64 +1,64 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
-import 'package:dart_code_algorithms/future_example/future_example.dart';
-import 'package:flutter/material.dart';
+// // ignore_for_file: public_member_api_docs, sort_constructors_first
+// import 'package:dart_code_algorithms/future_example/future_example.dart';
+// import 'package:flutter/material.dart';
 
-class ProfileDart extends StatefulWidget {
-  const ProfileDart({super.key, required this.timeset});
-  final int timeset;
+// class ProfileDart extends StatefulWidget {
+//   const ProfileDart({super.key, required this.timeset});
+//   final int timeset;
 
-  @override
-  State<ProfileDart> createState() => _ProfileDartState();
-}
+//   @override
+//   State<ProfileDart> createState() => _ProfileDartState();
+// }
 
-class _ProfileDartState extends State<ProfileDart> {
-  TotalTimeModel? model;
-  @override
-  void initState() {
-    model = TotalTimeModel(time: model?.time ?? 150);
-    super.initState();
-  }
+// class _ProfileDartState extends State<ProfileDart> {
+//   TotalTimeModel? model;
+//   @override
+//   void initState() {
+//     model = TotalTimeModel(time: model?.time ?? 150);
+//     super.initState();
+//   }
 
-  bool messageNotification = true;
-  late TextStyle? style =
-      Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white);
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white10,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const CircleAvatar(backgroundColor: Colors.red, radius: 50),
-            const SizedBox(
-              height: 20,
-            ),
-            Text(
-              "bilgiler",
-              style: style?.copyWith(fontSize: 30),
-            ),
-            Text(
-              "toplam zaman :  ${model?.time}",
-              style: style,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  "motivasyon mesajı",
-                  style: style,
-                ),
-                Switch(
-                    value: messageNotification,
-                    onChanged: (val) {
-                      messageNotification = val;
-                      setState(() {});
-                    })
-              ],
-            )
-          ],
-        ),
-      ),
-    );
-  }
-}
+//   bool messageNotification = true;
+//   late TextStyle? style =
+//       Theme.of(context).textTheme.bodyMedium?.copyWith(color: Colors.white);
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       backgroundColor: Colors.white10,
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             const CircleAvatar(backgroundColor: Colors.red, radius: 50),
+//             const SizedBox(
+//               height: 20,
+//             ),
+//             Text(
+//               "bilgiler",
+//               style: style?.copyWith(fontSize: 30),
+//             ),
+//             Text(
+//               "toplam zaman :  ${model?.time}",
+//               style: style,
+//             ),
+//             Row(
+//               mainAxisAlignment: MainAxisAlignment.center,
+//               children: [
+//                 Text(
+//                   "motivasyon mesajı",
+//                   style: style,
+//                 ),
+//                 Switch(
+//                     value: messageNotification,
+//                     onChanged: (val) {
+//                       messageNotification = val;
+//                       setState(() {});
+//                     })
+//               ],
+//             )
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
